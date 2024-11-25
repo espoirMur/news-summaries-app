93fcd4dddf1a6a876cd1340624e0463471232b12
gracebir <grace.devolop@gmail.com>
2024-11-25 14:23:00 +0200

    refactor hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,37 +1,32 @@
 <script setup>
+import heroImage from '@/assets/hero.png'
+import { RouterLink } from 'vue-router'
+
 defineProps({
-    imgUrl: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
     description: {
         type: String,
         required: true
     },
-    href: {
-        type: String,
+    id: {
+        type: Number,
         required: true
-    },
+    }
 })
 </script>
 
 <template>
     <div class="w-full rounded-xl lg:h-[50svh] min-h-[60svh] relative">
         <div class="w-full h-full">
-            <img class="w-full lg:h-[50svh] min-h-[60svh] object-cover rounded-xl" :src="imgUrl" alt="banner">
+            <img class="w-full lg:h-[50svh] min-h-[60svh] object-cover rounded-xl" :src="heroImage" alt="banner">
         </div>
         <div class="absolute top-0 right-0 left-0 bottom-0 bg-black rounded-xl px-8 bg-opacity-50 flex items-center">
             <div class="max-w-3xl flex flex-col gap-6 items-start text-gray-50">
-                <h1 class="text-2xl lg:text-3xl font-extrabold">{{ title }}</h1>
-                <p>{{ description }}</p>
-                <a target="_blank"
-                    class="text-gray-800 px-6 py-2 text-sm bg-gray-200 rounded-lg font-medium hover:bg-slate-900 hover:text-gray-100 duration-300"
-                    :href="href">Lire
-                    plus</a>
+                <p class="text-base font-semibold">{{ description.substring(0, 200) }}...</p>
+
+                <RouterLink :to="`/info/` + id"
+                    class="text-gray-800 px-6 py-2 text-sm bg-gray-200 rounded-lg font-medium hover:bg-slate-900 hover:text-gray-100 duration-300">
+                    Lire Plus
+                </RouterLink>
             </div>
         </div>
     </div>
--- src/components/NewTile.vue
+++ src/components/NewTile.vue
@@ -1,34 +1,30 @@
 <script setup>
-import moment from "moment"
+import { RouterLink } from "vue-router";
 defineProps({
-    title: {
-        type: String,
+    id: {
+        type: Number,
         required: true
     },
     description: {
         type: String,
         required: true
     },
-    published: {
-        type: String,
-        required: true
-    },
-    href: {
-        type: String,
-        required: true
-    },
+
+
 })
 </script>
 
 <template>
     <article class="w-full py-4 lg:py-6 border-b dark:border-gray-600 border-gray-200">
         <div class="flex flex-col gap-4 lg:gap-5 items-start">
-            <p class="text-sm">{{ moment(published).format("ll") }}</p>
-            <h3 class="text-xl lg:text-2xl font-extrabold">{{ title }}</h3>
-            <p class="text-base">{{ description }}</p>
-            <a target="_blank" class="text-sm font-semibold hover:underline cursor-pointer duration-300" :href="href">En
-                savoir plus
-                <span class="text-lg">&rsaquo;</span></a>
+            <!-- <p class="text-sm">{{ moment(published).format("ll") }}</p>
+            <h3 class="text-xl lg:text-2xl font-extrabold">{{ title }}</h3> -->
+            <p class="text-base">{{ description.substring(0, 250) }}...</p>
+            <RouterLink :to="`/info/` + id" class="text-sm font-semibold hover:underline cursor-pointer duration-300">
+                En savoir plus
+                <span class="text-lg">&rsaquo;</span>
+            </RouterLink>
+
         </div>
     </article>
 </template>
