8c88c37ef55363dd0f6b9d3be15ca5b5376ce309
gracebir <grace.devolop@gmail.com>
2024-11-25 14:24:17 +0200

    trying to config light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@
 
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: ["selector", '[data-mode="dark"]'],
     daisyui: {
         themes: ["light", "dark"],
     },
