9794095a32fa6a77562db015c8cf63323bbbd36b
gracebir <grace.devolop@gmail.com>
2024-12-20 12:33:16 +0200

    letter spacing

--- src/components/NewTile.vue
+++ src/components/NewTile.vue
@@ -28,7 +28,7 @@ const setDetailAndNavigate = () => {
     <article class="w-full py-4 lg:py-6 border-b dark:border-gray-600 border-gray-200">
         <div class="flex flex-col gap-4 lg:gap-5 items-start">
             <h3 class="text-xl lg:text-2xl font-extrabold">{{ data.titles[0] }}</h3>
-            <p class="text-base">{{ description }}</p>
+            <p class="text-base leading-7">{{ description }}</p>
             <RouterLink @click="setDetailAndNavigate" :to="`/info/` + id"
                 class="text-sm font-semibold  hover:underline cursor-pointer duration-300">
                 En savoir plus
