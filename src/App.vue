021cd808daef337f8a0c19eda5eeaebe89afadb4
gracebir <grace.devolop@gmail.com>
2024-12-21 16:22:48 +0200

    firebase

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import Header from './components/Header.vue';
-
-
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <Header />
   <RouterView />
   <footer class="w-full py-4 lg:py-6">
-    <div class="max-w-4xl mx-auto px-6 lg:px-8 font-montserrat flex justify-end ">
+    <div
+      class="max-w-5xl mx-auto px-6 lg:px-8 font-montserrat flex justify-end"
+    >
       <span class="text-base font-normal"> &copy; 2024</span>
     </div>
   </footer>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,22 @@
 <script setup>
-import { fetcher } from '@/api/fetcher';
-import NewTile from '@/components/NewTile.vue';
-import { useQuery } from '@tanstack/vue-query';
-import VueDatePicker from '@vuepic/vue-datepicker';
-import { ref, computed, watch } from 'vue';
+import { fetcher } from "@/api/fetcher";
+import NewTile from "@/components/NewTile.vue";
+import { useQuery } from "@tanstack/vue-query";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import { ref, computed, watch } from "vue";
 
 const selectedDate = ref(null);
 
 const formattedDate = computed(() => {
   if (!selectedDate.value) return null;
   const year = selectedDate.value.getFullYear();
-  const month = String(selectedDate.value.getMonth() + 1).padStart(2, '0');
-  const day = String(selectedDate.value.getDate()).padStart(2, '0');
+  const month = String(selectedDate.value.getMonth() + 1).padStart(2, "0");
+  const day = String(selectedDate.value.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 });
 
 const { data, isLoading, refetch } = useQuery({
-  queryKey: ['news', formattedDate],
+  queryKey: ["news", formattedDate],
   queryFn: () => fetcher(formattedDate.value),
   enabled: true, // Query will run automatically
 });
@@ -27,19 +27,34 @@ watch(formattedDate, () => {
 </script>
 
 <template>
-  <div v-if="isLoading" class="min-h-svh w-full mt-8 flex justify-center items-center">
+  <div
+    v-if="isLoading"
+    class="min-h-svh w-full mt-8 flex justify-center items-center"
+  >
     <span class="loading loading-spinner loading-md"></span>
   </div>
-  <main v-else-if="data" class="max-w-4xl mx-auto px-6 lg:px-8 font-montserrat flex flex-col gap-8">
+  <main
+    v-else-if="data"
+    class="max-w-5xl mx-auto px-6 lg:px-8 font-montserrat flex flex-col gap-8"
+  >
     <div class="flex justify-end">
       <div>
         <label for="">Trier par date</label>
-        <VueDatePicker class="text-gray-500" v-model="selectedDate"></VueDatePicker>
+        <VueDatePicker
+          class="text-gray-500"
+          v-model="selectedDate"
+        ></VueDatePicker>
       </div>
     </div>
     <div class="flex flex-col gap-4">
-      <NewTile v-for="(article, index) in data" :key="index" :data="article" :description="article.summary"
-        :published="article.publishedAt" :id="index" />
+      <NewTile
+        v-for="(article, index) in data"
+        :key="index"
+        :data="article"
+        :description="article.summary"
+        :published="article.publishedAt"
+        :id="index"
+      />
     </div>
   </main>
 </template>
