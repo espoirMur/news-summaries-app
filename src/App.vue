c5c591e9397ccedfc969471dcc52adb13a945706
gracebir <grace.devolop@gmail.com>
2025-01-05 18:25:30 +0200

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,10 @@ const currentYear = new Date().getFullYear();
       class="max-w-5xl mx-auto px-6 lg:px-8 font-montserrat flex justify-end"
     >
       <div class="flex flex-col gap-1">
-        <p class="text-sm font-semibold">
-          Design with love from Congo by Grace NSHOKANO
-        </p>
         <p class="text-sm font-semibold">Design with love ❤️ from 🇨🇩 💪🏿!</p>
-        <span class="text-base font-medium"> Balobi Nini &copy; {{ currentYear }}</span>
+        <span class="text-base font-medium">
+          Balobi Nini &copy; {{ currentYear }}</span
+        >
       </div>
     </div>
   </footer>
