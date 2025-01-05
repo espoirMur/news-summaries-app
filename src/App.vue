7189a1ceff7b2340ab3a904a17e841f4641fc06e
gracebir <grace.devolop@gmail.com>
2025-01-05 18:14:39 +0200

    new summary detail

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@ import Header from "./components/Header.vue";
   <Header />
   <RouterView />
   <footer class="w-full py-4 lg:py-6">
-    <div class="max-w-5xl mx-auto px-6 lg:px-8 font-montserrat flex justify-end">
-
+    <div
+      class="max-w-5xl mx-auto px-6 lg:px-8 font-montserrat flex justify-end"
+    >
       <div class="flex flex-col gap-1">
-        <p class="text-sm font-semibold">Design with love from Congo by Grace NSHOKANO</p>
-        <span class="text-base font-medium"> Balobi Nini &copy; 2024</span>
+        <p class="text-sm font-semibold">
+          Design with love from Congo by Grace NSHOKANO
+        </p>
+        <span class="text-base font-medium"> Balobi Nini &copy; 2025</span>
       </div>
     </div>
   </footer>
--- src/views/NewsDetail.vue
+++ src/views/NewsDetail.vue
@@ -1,65 +1,73 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 import { useNewsStore } from "@/stores/newsStore";
-import { computed, ref } from 'vue';
-import { extractTitleAndSummary } from '@/utils/funct';
+import { computed, ref } from "vue";
+import { extractTitleAndSummary } from "@/utils/funct";
 
-
-const LoadingSummaries = ref(false)
+const LoadingSummaries = ref(false);
 
 const newsStore = useNewsStore();
 
-
 const formattedNewsDetail = computed(() => {
-    if (!newsStore.news_detail) return null;
+  if (!newsStore.news_detail) return null;
 
-    const detail = newsStore.news_detail;
-    const { title, summary } = extractTitleAndSummary(detail.summary);
+  const detail = newsStore.news_detail;
+  const { summary } = extractTitleAndSummary(detail.summary);
 
-    return {
-        ...detail,
-        title: title || detail.titles[0],
-        summary,
-    };
+  return {
+    ...detail,
+    title: detail.title ? detail.title : detail.titles[0],
+    summary,
+  };
 });
 
 newsStore.loadNewsDetailFromStorage();
-
 </script>
 <template>
-    <main class="max-w-5xl mx-auto px-6 lg:px-8 mt-8  min-h-[80svh] font-montserrat">
-        <div v-if="LoadingSummaries" class="w-full flex min-h-[50svh] justify-center items-center">
-            <span class="loading loading-spinner loading-md"></span>
+  <main
+    class="max-w-5xl mx-auto px-6 lg:px-8 mt-8 min-h-[80svh] font-montserrat"
+  >
+    <div
+      v-if="LoadingSummaries"
+      class="w-full flex min-h-[50svh] justify-center items-center"
+    >
+      <span class="loading loading-spinner loading-md"></span>
+    </div>
+    <div v-else-if="formattedNewsDetail" class="grid grid-cols-1 gap-8">
+      <div class="flex flex-col items-start">
+        <RouterLink
+          to="/"
+          class="flex items-center flex-row text-sm px-6 py-2 rounded-md text-gray-200 bg-gray-700 hover:bg-gray-800"
+        >
+          Retour en arrieure
+        </RouterLink>
+      </div>
+      <div class="flex flex-col gap-4 lg:gap-6 col-span-1">
+        <h3 class="text-2xl md:text-3xl font-bold">
+          {{ formattedNewsDetail.title }}
+        </h3>
+        <p class="text-sm md:text-base leading-6">
+          {{ formattedNewsDetail.summary }}
+        </p>
+      </div>
+      <div class="max-w-4xl flex flex-col gap-3">
+        <h5 class="text-base md:text-lg font-bold">Sources:</h5>
+        <div>
+          <ul class="ml-4 flex flex-col gap-2">
+            <li v-for="(value, key) in formattedNewsDetail.urls" :key="key">
+              <span class="font-bold">- </span>
+              <a
+                :href="value"
+                class="hover:underline font-medium"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {{ formattedNewsDetail.titles[key] }}
+              </a>
+            </li>
+          </ul>
         </div>
-        <div v-else-if="formattedNewsDetail" class="grid grid-cols-1 gap-8">
-            <div class="flex flex-col items-start">
-                <RouterLink to="/"
-                    class="flex items-center flex-row text-sm px-6 py-2 rounded-md text-gray-200 bg-gray-700 hover:bg-gray-800">
-                    Retour
-                    en
-                    arrieure
-                </RouterLink>
-            </div>
-            <div class="flex flex-col gap-4 lg:gap-6 col-span-1 ">
-                <h3 class="text-2xl md:text-3xl font-bold">{{ formattedNewsDetail.title }}</h3>
-                <p class="text-sm md:text-base leading-6">{{ formattedNewsDetail.summary }}</p>
-            </div>
-            <div class="max-w-4xl flex flex-col gap-3">
-
-                <h5 class="text-base md:text-lg font-bold">Sources:</h5>
-                <div>
-                    <ul class="ml-4 flex flex-col gap-2">
-                        <li v-for="(value, key) in formattedNewsDetail.urls" :key="key">
-                            <span class="font-bold">- </span>
-                            <a :href="value" class="hover:underline font-medium" target="_blank"
-                                rel="noopener noreferrer">
-                                {{ formattedNewsDetail.titles[key] }}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
